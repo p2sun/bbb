b1734e5e39e321ffb5d69d8f22f8a1d708c71416
System Administrator <root@v1020-wn-157-102.campus-dynamic.uwaterloo.ca>
2018-03-16 21:19:38 -0400

    Parsing STATUS_OUT wip

--- clientpy2.py
+++ clientpy2.py
@@ -1,11 +1,26 @@
 import socket
 import sys
     
+# Status dictionary 
+StatusDict = {
+  "COMMAND": "",
+  "X": "",
+  "Y": "",
+  "DX": "",
+  "DY" : "",
+  "NUM_MINES": "",
+  "NUM_PLAYERS": "",
+  "PLAYER_POS": [],
+  "NUM_BOMBS": "",
+  "NUM_WORMHOLES": "",
+  "WORMHOLES": []
+}
+
 def run(user, password, *commands):
     HOST, PORT = "codebb.cloudapp.net", 17429
     
     data=user + " " + password + "\n" + "\n".join(commands) + "\nCLOSE_CONNECTION\n"
-    
+    returnList = [] 
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -14,10 +29,12 @@ def run(user, password, *commands):
         sfile = sock.makefile()
         rline = sfile.readline()
         while rline:
+	    returnList.append(rline.strip())
             print(rline.strip())
             rline = sfile.readline()
     finally:
         sock.close()
+    return returnList
 
 def subscribe(user, password):
     HOST, PORT = "codebb.cloudapp.net", 17429
@@ -32,10 +49,43 @@ def subscribe(user, password):
         sfile = sock.makefile()
         rline = sfile.readline()
         while rline:
-            print(rline.strip())
+            #print(rline.strip())
             rline = sfile.readline()
     finally:
         sock.close()
 
 #TESTING
-run("Theboys", "vmarutha", "STATUS")
+status = run("Theboys", "vmarutha", "STATUS")
+for s in xrange(len(status)):
+	test = dict(StatusDict)
+	splitStatus = status[s].split()
+	print splitStatus
+	for x in xrange(len(splitStatus)):
+		print x
+		if (x == 0):
+			test["COMMAND"] = splitStatus[x]
+		elif (x == 1):
+			test["X"] = splitStatus[x]
+		elif (x == 2):
+			test["Y"] = splitStatus[x]
+		elif (x == 3):
+			test["DX"] = splitStatus[x]
+		elif (x == 4):
+			test["DY"] = splitStatus[x]
+		elif (x == 6):
+			test["NUM_MINES"] = splitStatus[x]
+	print test
+
+StatusDict = {
+  "COMMAND": "",
+  "X": "",
+  "Y": "",
+  "DX": "",
+  "DY" : "",
+  "NUM_MINES": "",
+  "NUM_PLAYERS": "",
+  "PLAYER_POS": [],
+  "NUM_BOMBS": "",
+  "NUM_WORMHOLES": "",
+  "WORMHOLES": []
+}
